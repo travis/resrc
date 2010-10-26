77b150a054ee8c39a71c11e720099edb86538590
Travis Vachon <travis@weatherbill.com>
2010-10-26 09:33:33 -0400

    Remove dev dependencies - they should be installed as user plugins.

--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   :description "resrc is a Clojure library for creating RESTful APIs"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [clout "0.3.1"]]
-  :dev-dependencies [[swank-clojure "1.2.0"]])
+                 [clout "0.3.1"]])
