5331cc044632488b61e939ee71e3812daec3a810
Travis Vachon <travis.vachon@gmail.com>
2011-01-14 00:15:40 -0500

    Flesh out Resource with the rest of rfc2616 methods
    Support the methods specified in http://www.ietf.org/rfc/rfc2616.txt

--- src/resrc/core.clj
+++ src/resrc/core.clj
@@ -3,6 +3,11 @@
 
 (defprotocol Resource
   (GET [resource] [resource request])
+  (HEAD [resource] [resource request])
   (PUT [resource] [resource request])
   (DELETE [resource] [resource request])
-  (POST [resource] [resource request]))
+  (POST [resource] [resource request])
+
+  (OPTIONS [resource] [resource request])
+  (TRACE [resource] [resource request])
+  (CONNECT [resource] [resource request]))
